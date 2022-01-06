975912247c189bdf9061ca64ee1a83dbd45b91f8
Amin Berneti <amin.berneti@cubic.com>
2022-01-06 12:28:39 +0100

    Initial project setup

---
+++ src/main/java/com/cubic/umohackathonblockchain/UmoHackathonBlockchainApplication.java
@@ -0,0 +1,13 @@
+package com.cubic.umohackathonblockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmoHackathonBlockchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmoHackathonBlockchainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cubic/umohackathonblockchain/UmoHackathonBlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cubic.umohackathonblockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmoHackathonBlockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
